cb4bc68438bd00af072a46960aa85fe6441945f6
Brian Pruitt-Goddard <bpruitt-goddard@firstam.com>
2022-12-09 07:06:25 -0800

    feat: add initial csharp code and invalid cash test

--- PaymentService.Tests/ProcessPaymentTests.cs
+++ PaymentService.Tests/ProcessPaymentTests.cs
@@ -11,9 +11,11 @@ public class UnitTest1
         var invalidAmount = -4;
 
         var res_fs = PaymentService_FS.processPaymentCash(invalidAmount);
+        var res_cs = PaymentService_CS.ProcessPaymentCash(invalidAmount);
 
         var expectedResponse = "Invalid payment: Cash must be a positive amount";
         Assert.Equal(expectedResponse, res_fs);
+        Assert.Equal(res_fs, res_cs);
     }
 
     [Fact]
--- Payment_CS/PaymentService.cs
+++ Payment_CS/PaymentService.cs
@@ -1,9 +1,49 @@
 ﻿namespace Payment_CS;
 
+public abstract class PaymentBase
+{
+	public decimal Amount { get; set; }
+	public string Error { get; set; }
+	public abstract bool IsValid();
+	public abstract string GetPaymentSummary();
+}
+public class CashPayment : PaymentBase
+{
+	public CashPayment(decimal amount)
+	{
+		Amount = amount;
+	}
+
+	public override bool IsValid()
+	{
+		if (Amount >= 0)
+			return true;
+
+		Error = "Cash must be a positive amount";
+		return false;
+	}
+
+	public override string GetPaymentSummary() =>
+		$"Cash payment of {Amount} received.";
+}
+
 public static class PaymentService_CS
 {
-	public static string ProcessPayment()
+	public static string ProcessPayment(PaymentBase payment)
 	{
-		return "hi";
+		if (payment is null)
+		{
+			return "Invalid payment";
+		}
+
+		if (payment.IsValid())
+		{
+			return payment.GetPaymentSummary();
+		}
+
+		return $"Invalid payment: {payment.Error}";
 	}
+
+	public static string ProcessPaymentCash(decimal amount) =>
+		ProcessPayment(new CashPayment(amount));
 }
